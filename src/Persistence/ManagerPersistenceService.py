1a05b0a9d78dfd85eea5a40ced6670c8da4f73b0
Jason Xie <xxie95@uwo.ca>
2022-11-28 17:17:41 -0500

    push to the jasonx branch, restructure the project, currently working on the persistence

--- src/Persistence/ManagerPersistenceService.py
+++ src/Persistence/ManagerPersistenceService.py
@@ -19,7 +19,9 @@ class ManagerPersistenceService:
         cur = dbConnection.connection.cursor()
 
         try:
-            cur.execute("UPDATE hoursWorked SET hourAmount = " + workHours + "WHERE employeeID = " + teamEmployeeId)
+            cur.execute("UPDATE hoursWorked SET hourAmount = '%(1)s' WHERE employeeID = '%(2)s';" % {"1" : workHours, "2" : teamEmployeeId})
+
+        # FIXME store the query results into data objects
         except mysql.connector.Error as error:
             print(error)
             sys.exit(1)
@@ -30,21 +32,49 @@ class ManagerPersistenceService:
     """
     @staticmethod
     def getSummaryTeamEmployeeData(teamId):
-        pass
+        cur = dbConnection.connection.cursor()
+
+        try:
+            cur.execute("SELECT * FROM team WHERE teamID = '%(1)s';" % {"1" : teamId})
+        # FIXME store the query results into data objects
+
+        except mysql.connector.Error as error:
+            print(error)
+            sys.exit(1)
+        dbConnection.connection.close()
 
     """
     Get Specific Team Employee's work related data
     """
     @staticmethod
-    def getTeamEmployeeWorkData(teamEmployeeId):
-        pass
+    def getTeamEmployeeWorkData(teamID, teamEmployeeID):
+        cur = dbConnection.connection.cursor()
+
+        try:
+            cur.execute("SELECT fname, lname, projectName FROM project, team, employee WHERE employee.employeeID = teamEmployeeID AND ")
+            # FIXME store the query results into data objects
+
+        except mysql.connector.Error as error:
+            print(error)
+            sys.exit(1)
+        dbConnection.connection.close()
 
     """
     Assign Employee to team
     """
     @staticmethod
     def assignEmployeeToTeam(teamEmployeeId, managerId):
-        pass
+        cur = dbConnection.connection.cursor()
+
+        try:
+            # FIXME wrong query so far, will fix it later
+            cur.execute("INSERT INTO team () '%(1)s';" % {"1": managerId})
+            # FIXME store the query results into data objects
+
+        except mysql.connector.Error as error:
+            print(error)
+            sys.exit(1)
+        dbConnection.connection.close()
 
     """
     Remove Employee from team
@@ -64,5 +94,14 @@ class ManagerPersistenceService:
     Get project details
     """
     @staticmethod
-    def getProjectDetails():
-        pass
+    def getProjectDetails(projectID):
+        cur = dbConnection.connection.cursor()
+
+        try:
+            cur.execute("SELECT * FROM project WHERE projectID = '%(1)s';" % {"1": projectID})
+        # FIXME store the query results into data objects
+
+        except mysql.connector.Error as error:
+            print(error)
+            sys.exit(1)
+        dbConnection.connection.close()
