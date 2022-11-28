3a2e8c269bfb890fed732b0cd3f24588970beda7
Matthew <33932793+GermanBeast7@users.noreply.github.com>
2022-11-28 12:10:03 -0500

    Add Attributes to Data Classes

--- Structure Python Classes/Data_Objects/Manager.py
+++ Structure Python Classes/Data_Objects/Manager.py
@@ -3,3 +3,13 @@ from Data_Objects.Employee import Employee
 Manager class for methods of employee and attributes
 """
 class Manager(Employee):
+    def __init__(self, employeeId, password, empType, teamId, managerId, fName, lName, salary, position, startDate,
+                 birthDate, sickDaysYearly, sickDaysRemaining, vacationDaysYearly, vacationDaysRemaining,
+                 address, phoneNumber, workEmail, personalEmail, directDepositNumber, ssn, employeesManaged,
+                 teamManaged):
+        super().__init__(self, employeeId, password, empType, teamId, managerId, fName, lName, salary, position,
+                         startDate, birthDate, sickDaysYearly, sickDaysRemaining, vacationDaysYearly,
+                         vacationDaysRemaining,address, phoneNumber, workEmail, personalEmail, directDepositNumber,
+                         ssn)
+        self.employeesManaged = employeesManaged
+        self.teamManaged = teamManaged
