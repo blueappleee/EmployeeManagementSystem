d13ec0d499828a7b9e4d89e7c1cefac3f788ed99
Connor Ciavarella <cciavare@uwo.ca>
2022-11-29 16:44:12 -0500

    ADD partial implementation of persistence classes

--- src/Persistence/AdminPersistenceService.py
+++ src/Persistence/AdminPersistenceService.py
@@ -15,7 +15,21 @@ class AdminPersistenceService:
     """
     @staticmethod
     def searchAdminById(adminId) -> SysAdmin:
-        pass
+        cur = dbConnection.connection.cursor()
+
+        try:
+            query='SELECT * FROM employee WHERE employeeID="' employeeId + '"'
+            cur.execute(query)
+            result=cur.fetchall()
+            sysAdmin = SysAdmin(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9],
+                 result[10], result[11], result[12], result[13], result[14],
+                 result[15], result[16], result[17], result[18], result[19], result[20])
+        except mysql.connector.Error as error:
+            print(error)
+            sys.exit(1)
+        dbConnection.connection.close()
+        
+        return sysAdmin
 
     """
     Update Employee Role
@@ -29,12 +43,54 @@ class AdminPersistenceService:
     """
     @staticmethod
     def registerNewEmployee(employee: Employee):
-        pass
+        cur = dbConnection.connection.cursor()
+        teamI=""
+        manI=""
+        
+        try:
+            query='INSERT INTO employee (employeeID, password, empType, teamID, managerID, fname, lname, salary, position, startDate, birthDate, sickDaysYearly, sickDaysRemaining, vacationDaysYearly, vacationDaysRemaining, address, phonenumber, workEmail, personalEmail, directDepositNumber, ssn) VALUES ("'
+                   + employee.employeeId + '", "' + employee.password + '", "' + employee.empType + '", "' + employee.teamId + '", "' + employee.managerId + '", "'
+                   + employee.fname + '", "' employee.lname + '", "' + employee.salary + '", "' + employee.position + '", "' + employee.startDate.strftime('%Y-%m-%d') + '", "'
+                   + employee.birthDate.strftime('%Y-%m-%d') + '", "' + employee.sickDaysYearly + '", "' + employee.sickDaysRemaining + '", "' + employee.vacationDaysYearly + '", "'
+                   + employee.vacationDaysRemaining + '", "' + employee.address + '", "' + employee.phonenumber + '", "' + employee.phonenumber + '", "' + employee.workEmail + '", "'
+                   + employee.personalEmail + '", "' + employee.directDepositNumber + '", "' + employee.ssn + '")'
+            cur.execute(query)
+            dbConnection.connection.commit()
+            empI=cur.rowcount
+        except mysql.connector.Error as error:
+            # error if employeeId already taken or not null value isnt set
+            print(error)
+            return error
+            
+        if employee.teamId != null:
+            try:
+                query='INSERT INTO teamMembers (teamID, employeeID) VALUES ("' + employee.teamId + '" , "' employee.employeeId + '")'
+                cur.execute(query)
+                dbConnection.connection.commit()
+                teamI=cur.rowcount
+            except mysql.connector.Error as error:
+                # error if team doesnt exist
+                print(error)
+                return error
+            
+        if managerID != null:
+            try:
+                query='INSERT INTO empManaged (managerID, employeeID) VALUES ("' + employee.managerId + '" , "' employee.employeeId + '")'
+                cur.execute(query)
+                dbConnection.connection.commit()
+                manI=cur.rowcount
+            except mysql.connector.Error as error:
+                # error if team doesnt exist
+                print(error)
+                return error
+            
+        dbConnection.connection.close()
+        return (empI, teamI, manI)
 
     """
     Make active employee inactive
     """
-    @staticmethod
+    @staticmethod 
     def setEmployeeInactive(employeeId):
         pass
 
@@ -43,18 +99,141 @@ class AdminPersistenceService:
     """
     @staticmethod
     def assignManagerToTeam(managerId, teamId):
-        pass
+        # will return error if team doesnt already exist
+        cur = dbConnection.connection.cursor()
+
+        try:
+            query='UPDATE team SET teamManagerID="' + teamId + '" WHERE employeeID="' + manager.employeeId + '"'
+            cur.execute(query)
+            dbConnection.connection.commit()
+            teamI= cur.rowcount
+        except mysql.connector.Error as error:
+            print(error)
+            return error
+            
+            
+        try:
+            query='UPDATE employee SET teamID="' + teamId + '" WHERE employeeID="' + manager.employeeId + '"'
+            cur.execute(query)
+            dbConnection.connection.commit()
+            empI= cur.rowcount
+        except mysql.connector.Error as error:
+            print(error)
+            return error
+              
+        try:
+            query='SELECT * FROM teamMembers WHERE employeeID="' manager.employeeId + '"'
+            cur.execute(query)
+            teamRowExists = False
+            memberResult = cur.fetchall()
+            teamMemI=""
+            
+            for row in memberResult:
+                if row[0] != teamId:
+                    dropQuery = 'Delete FROM teamMembers WHERE employeeID="' + manager.employeeId + '" AND teamID="' + teamId + '"'
+                    cur.execute(sql)
+                    dbConnection.connection.commit()
+                else:
+                    teamRowExists=True
+                    
+            if teamRowExists==False:
+                try:
+                    query='INSERT INTO teamMembers (teamID, employeeID) VALUES ("' + teamId + '", "' + manager.employeeId +  '")'
+                    cur.execute(query)
+                    dbConnection.connection.commit()
+                    teamMemI = cur.rowcount
+                except mysql.connector.Error as error:
+                    print(error)
+                    return error
+            
+        except mysql.connector.Error as error:
+            print(error)
+            return error
+        
+        try:
+            query='SELECT * FROM teamManaged WHERE managerID="' manager.employeeId + '"'
+            cur.execute(query)
+            teamManRowExists = False
+            managedResult = cur.fetchall()
+            teamManI=""
+            
+            for row in managedResult:
+                if row[1] != teamId:
+                    dropQuery = 'Delete FROM teamManaged WHERE managerID="' + manager.employeeId + '" AND teamID="' + teamId + '"'
+                    cur.execute(sql)
+                    dbConnection.connection.commit()
+                else:
+                    teamManRowExists=True
+                    
+            if teamManRowExists==False:
+                try:
+                    query='INSERT INTO teamManaged (managerID, teamID) VALUES ("' + manager.employeeId + '", "' + teamId +  '")'
+                    cur.execute(query)
+                    dbConnection.connection.commit()
+                    teamManI = cur.rowcount
+                except mysql.connector.Error as error:ject
+                    print(error)
+                    return error
+        except mysql.connector.Error as error:
+            print(error)
+            return error
+        
+        
+        dbConnection.connection.close()
+        return (empI, teamMemI, teamManI)
 
     """
     Create new record in Team table for team with data stored in Team object
     """
     @staticmethod
     def createTeam(team: Team):
-        pass
+        cur = dbConnection.connection.cursor()
 
+        try:
+            query='INSERT INTO team (teamID, teamManagerID, teamName, projectID) VALUES ("'
+                   + team.teamId + '", "' + team.teamManagerID + '", "' + team.teamName + '", "' + team.projectId + '")'
+            cur.execute(query)
+            dbConnection.connection.commit()
+            teamI = cur.rowcount
+        except mysql.connector.Error as error:
+            # potential errors: project needs to exist before adding team with an active project
+            print(error)
+            return error
+            
+        membersI = []
+        for member in team.teamMembers:
+            try:
+                query='INSERT INTO team (teamID, teamManagerID, teamName, projectID) VALUES ("'
+                       + team.teamId + '", "' + team.teamManagerID + '", "' + team.teamName + '", "' + team.projectId + '")'
+                cur.execute(query)
+                dbConnection.connection.commit()
+                membersI.append(cur.rowcount)
+            except mysql.connector.Error as error:
+                # potential errors: project needs to exist before adding team with an active project
+                print(error)
+                return error
+        
+        dbConnection.connection.close()
+        return (teamI, membersI)
+        
     """
     Create new record in Project table for project with data stored in Project object
     """
     @staticmethod
     def createProject(project: Project):
-        pass
+        cur = dbConnection.connection.cursor()
+
+        try:
+            query='INSERT INTO project (projectID, projectName, currentTeamID, projectStatus) VALUES ("'
+                   + project.projectId + '", "' + project.projectName + '", "' + project.currentTeamId + '", "' + project.projectStatus + '")'
+            cur.execute(query)
+            dbConnection.connection.commit()
+            return cur.rowcount
+        except mysql.connector.Error as error:
+            # potential errors: team does not exist
+            print(error)
+            return error
+            
+        # TODO team if teamID is set
+            
+        dbConnection.connection.close()
--- src/Persistence/EmployeePersistenceService.py
+++ src/Persistence/EmployeePersistenceService.py
@@ -11,18 +11,75 @@ class EmployeePersistenceService:
     """
     @staticmethod
     def searchEmployeeById(employeeId) -> Employee:
-        pass
+        cur = dbConnection.connection.cursor()
+
+        try:
+            query='SELECT * FROM employee WHERE employeeID="' employeeId + '"'
+            cur.execute(query)
+            result=cur.fetchall()
+            employee = Employee(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9],
+                 result[10], result[11], result[12], result[13], result[14],
+                 result[15], result[16], result[17], result[18], result[19], result[20])
+        except mysql.connector.Error as error:
+            print(error)
+            sys.exit(1)
+        dbConnection.connection.close()
+        
+        return employee
 
     """
     Update Employee record in Employee Table
     """
     @staticmethod
     def updateEmployeeInformation(employee: Employee):
-        pass
+        cur = dbConnection.connection.cursor()
+
+        try:
+            query='UPDATE employee SET password="' + employee.password
+            + '", empType="' + employee.empType
+            + '", teamID="' + employee.teamId
+            + '", managerID="' + employee.managerId
+            + '", fname="' + employee.fname 
+            + '", lname="' + employee.lname
+            + '", salary="' + employee.salary
+            + '", position="' + employee.position
+            + '", startDate="' + employee.startDate
+            + '", birthDate="' + employee.birthDate
+            + '", sickDaysYearly="' + employee.sickDaysYearly
+            + '", sickDaysRemaining="' + employee.sickDaysRemaining
+            + '", vacationDaysYearly="' employee.vacationDaysYearly
+            + '", vacationDaysRemaining="' employee.vacationDaysRemaining
+            + '", address="' + employee.address
+            + '", phonenumber="' + employee.phonenumber
+            + '", workEmail="' + employee.workEmail
+            + '", personalEmail="' employee.personalEmail
+            + '", directDepositNumber="' + employee.directDepositNumber
+            + '", ssn="' + employee.ssn + '" WHERE employeeID="' + employee.employeeId + '"'
+            cur.execute(query)
+            mydb.commit()
+        except mysql.connector.Error as error:
+            print(error)
+            sys.exit(1)
+            
+        # do teamMember and empManagerd query as well to ensure set properly    
+        dbConnection.connection.close()
 
     """
     Update Employee's work hours
     """
     @staticmethod
-    def logWorkHours(employee: Employee, workType, workTime):
-        pass
+    def logWorkHours(employee: Employee, workType, workTime, workDate):
+        cur = dbConnection.connection.cursor()
+
+        try:
+            query='INSERT INTO hoursWorked ("employeeID", "hourType", "hourAmount", "workDate") VALUES ("' + Employee.employeeID + '", "' + workType + '", "' + workTime + '", "' + workDate + '")'
+            cur.execute(query)
+            mydb.commit()
+            hoursWorkedR = cur.rowcount
+            
+        except mysql.connector.Error as error:
+            print(error)
+            return error
+            
+        # need to add to team hours worked
+        dbConnection.connection.close()
